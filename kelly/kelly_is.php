695160c2b59911dd032f560c4b8ef441361b746b
kimyh5392 <mk.kimyh@gmail.com>
2017-03-13 18:42:53 +0900

    kelly > kellyis.php, location > map.php , reservation > reserve_write.php 완료

---
+++ kelly/kelly_is.php
@@ -0,0 +1,43 @@
+<?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/dochead.php'); ?>
+<link href="/assets/css/sub.css" rel="stylesheet">
+<link href="/assets/css/kelly.css" rel="stylesheet">
+</head>
+<body class="sub kelly">
+    <?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/header.php'); ?>
+    <main class="content">
+        <div class="container">
+            <!-- page-header -->
+            <div class="page-header">
+                <h2>KELLY IS</h2>
+                <p>도심 속, 자연과 어우러진 하우스웨딩이<br class="visible-xs"> 당신의 꿈꾸는 결혼을 실현시켜드립니다.</p>
+            </div>
+            <section class="kelly-is">
+                <div class="section-header">
+                    <h3>
+                        <img src="/assets/images/kelly/kelly_is_header.png" alt="kelly is images">
+                    </h3>
+                    <p>
+                        함께하는 아름다운 첫 걸음, The Grace Kelly는<br class="visible-xs"> 당신과 함께 행복의<br class="hidden-xs"> 시작을 동행합니다.도심 속,<br class="visible-xs"> 자연과 어우러진 하우스웨딩이 당신의<br> 꿈꾸는결혼을실현시켜드립니다. 당신이<br class="visible-xs"> 그려온둘만의 특별한<br class="hidden-xs"> 이야기가 The Grace Kelly<br class="visible-xs">과만나 이전에 볼 수 없었던<br class="hidden-xs"> 웨딩스타일링을<br class="visible-xs"> 선물합니다.
+                    </p>
+                </div>
+                <div class="section-content">
+                    <div class="content-header">
+                        <hr>
+                        <h4>NOW AND HERE</h4>
+                        <p>
+                            모든 것이 지금 여기에서 시작합니다.도심 속 야외 정원에서 올리는 정통 HOUSE WEDDING 나만의 예식,<br class="visible-sm"> 우리만의 PRIVATE WEDDING,<br class="visible-md visible-lg"> 여유롭게 즐길 수 있는 ENJOY WEDDING, 우리집 정원과 거실로 초대하는 COMFORT WEDDING,<br class="visible-sm"> GRACEKELLY 만의 SPECIAL WEDDING,<br class="visible-md visible-lg"> GRACEKELLY와 신랑 신부가 직접 만드는 MAKING WEDDING<br class="visible-sm"> 더 그레이스켈리에서 약속드립니다
+                        </p>
+                    </div>
+                    <figure>
+                        <img src="/assets/images/kelly/kelly_is_sm_visual.jpg" class="img-responsive visible-xs" alt="kelly is images">
+                        <img src="/assets/images/kelly/kelly_is_md_visual.jpg" class="img-responsive visible-sm" alt="kelly is images">
+                        <img src="/assets/images/kelly/kelly_is_lg_visual.jpg" class="img-responsive visible-md visible-lg" alt="kelly is images">
+                    </figure>
+                </div>
+            </section>
+        </div>
+    </main>
+    <?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/footer.php'); ?>
+    <?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/docfoot.php'); ?>
+</body>
+</html>
---
+++ reservation/reserve_write.php
@@ -0,0 +1,143 @@
+<?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/dochead.php'); ?>
+<link href="/assets/css/sub.css" rel="stylesheet">
+<link href="/assets/css/reservation.css" rel="stylesheet">
+<link href="//code.jquery.com/ui/1.12.0/themes/base/jquery-ui.css" rel="stylesheet">
+</head>
+<body class="sub reservation">
+    <?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/header.php'); ?>
+    <main class="content">
+        <div class="container">
+            <!-- page-header -->
+            <div class="page-header">
+                <h2>RESERVATION</h2>
+                <p>원하시는 서비스에 친절한 안내를 받으실 수 있습니다</p>
+            </div>
+            <article class="reserve_write">
+                <form action="#">
+                    <div class="service-info">
+                        <div class="service">
+                            정보통신망 법 제 50조의 2 (전자우편주소의 무단 수집행위 등 금지)<br><br>
+                            1. 누구든지 전자우편주소의 수집을 거부하는 의사 인터넷 홈페이지에서 자동으로 전자우편주소를 수집 프로그램 그 밖의 기술적 장치를 이용하여 전자우편주수집하여서는 아니된다.
+                        </div>
+                        <div class="form-group">
+                            <label><input type="checkbox">개인정보수집에 동의합니다.</label>
+                        </div>
+                    </div>
+                    <section class="write">
+                        <h3>예약정보 입력</h3>
+                        <div class="table-wrap">
+                            <table class="table table-bordered">
+                                <tbody>
+                                    <tr>
+                                        <th>행사희망일정</th>
+                                        <td class="date">
+                                            <div class="form-group">
+                                                <input type="text" id="u-date" class="form-control">
+                                                <label for="u-date">
+                                                    <i class="icon-calendar"></i>
+                                                    <span class="sr-only">날짜조회</span>
+                                                </label>
+                                            </div>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <th><label for="guest-sum">예상하객수</label></th>
+                                        <td>
+                                            <div class="form-group">
+                                                <select id="guest-sum" class="form-control">
+                                                    <option>선택하세요</option>
+                                                    <option>100명</option>
+                                                    <option>150명</option>
+                                                    <option>200명</option>
+                                                </select>
+                                            </div>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <th><label for="u-name">신청자성명</label></th>
+                                        <td>
+                                            <div class="form-group">
+                                                <input type="text" id="u-name" class="form-control">
+                                            </div>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <th><label for="u-mail">이메일주소</label></th>
+                                        <td>
+                                            <div class="form-group">
+                                                <input type="email" id="u-mail" class="form-control">
+                                            </div>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <th><label for="u-phone">휴대전화</label></th>
+                                        <td>
+                                            <div class="form-group">
+                                                <input type="text" id="u-phone" class="form-control">
+                                            </div>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <th><label for="u-pw">비밀번호</label></th>
+                                        <td>
+                                            <div class="form-group">
+                                                <input type="text" id="u-pw" class="form-control">
+                                            </div>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <th class="prevent"><label for="captcha">자동등록방지</label></th>
+                                        <td class="captcha">
+                                            <div class="captcha-box">
+                                                자동등록방지 영역
+                                            </div>
+                                            <div class="form-group">
+                                                <input type="text" id="captcha" class="form-control">
+                                            </div>
+                                            <p class="attention">자동등록방지 숫자를 순서대로 입력하세요</p>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <th class="u-content"><label for="u-content">상담받고자 하는내용</label></th>
+                                        <td>
+                                            <div class="form-group">
+                                                <textarea id="u-content" class="form-control"></textarea>
+                                            </div>
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                        <div class="btn-area">
+                            <p>
+                                <button type="submit" class="btn btn-gray">신청하기</button>
+                            </p>
+                        </div>
+                    </section>
+                </form>
+            </article>
+        </div>
+    </main>
+    <?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/footer.php'); ?>
+    <?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/docfoot.php'); ?>
+    <script src="https://code.jquery.com/ui/1.12.0/jquery-ui.js"></script>
+    <script>
+        (function($) {
+            $("#u-date").datepicker({
+                dateFormat: 'yy-mm-dd',
+                prevText: '이전 달',
+                nextText: '다음 달',
+                monthNames: ['1월','2월','3월','4월','5월','6월','7월','8월','9월','10월','11월','12월'],
+                monthNamesShort: ['1월','2월','3월','4월','5월','6월','7월','8월','9월','10월','11월','12월'],
+                dayNames: ['일','월','화','수','목','금','토'],
+                dayNamesShort: ['일','월','화','수','목','금','토'],
+                dayNamesMin: ['일','월','화','수','목','금','토'],
+                showMonthAfterYear: true,
+                changeMonth: true,
+                changeYear: true,
+                yearSuffix: '년'
+            });
+        })(jQuery);
+    </script>
+</body>
+</html>
